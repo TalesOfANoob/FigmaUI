6914f641a7a1a8f0e051b5a51f62e3dcaea6aa08
TalesOfANoob <44920433+TalesOfANoob@users.noreply.github.com>
2020-11-07 22:01:52 +0200

    svg circle

--- src/core/components/AccountScreenComponents/AccountContent.js
+++ src/core/components/AccountScreenComponents/AccountContent.js
@@ -1,10 +1,18 @@
+/* eslint-disable no-shadow */
 /* eslint-disable prettier/prettier */
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import {View,Text,ScrollView} from 'react-native';
 import {AccountContentStyle as styles} from './styles';
 import strings from './static/strings';
-import {Svg,Text as SvgText} from 'react-native-svg';
+import Svg,{Text as SvgText,Circle} from 'react-native-svg';
+import { RFValue } from 'react-native-responsive-fontsize';
 const AccountContent = () => {
+
+    const size = 65;
+    const strokeWidth = 15;
+    const radius = size - strokeWidth;
+    const circumference = 2 * Math.PI * radius;
+
     const mockData = {
         estadistics:{
             level:30,
@@ -13,25 +21,60 @@ const AccountContent = () => {
 
         },
     };
+    const [level,setLevel] = useState(0);
+    useEffect(()=>{
+        setInterval(() => {
+
+            setLevel(l=>l < 100 ? l + 0.5 : 0);
+
+        }, 10);
+    },[]);
+
+    const fillLevel = ( level * circumference) / 100;
+
     return (
     <View style={styles.container}>
         <Text style={styles.goldHeader}>
             {strings.findHome}
         </Text>
         <ScrollView
+
             style={styles.statsList}
          >
-            <View>
-                <Text style={styles.estatistics}>{strings.estadistics}</Text>
-                <Svg>
-                    <SvgText
-                    >
-                    {mockData.estadistics.level}
-                    </SvgText>
+            <View >
+                <Text
+                    style={styles.estatistics}>
+                    {strings.estadistics}
+                </Text>
+
+                <Svg style={{width:size * 2,height:size * 2}}>
+
+                    <Circle
+                        stroke="#e3e3e3"
+                        fill="none"
+                        r={radius}
+                        cx={size}
+                        cy={size}
+                        strokeWidth={strokeWidth}
+                        strokeDasharray={`${circumference}`}
+                    />
+                    <Circle
+
+                        stroke="#46D0D9"
+                        fill="none"
+                        r={radius}
+                        cx={size}
+                        cy={size}
+                        strokeWidth={strokeWidth}
+                        strokeDasharray={`${fillLevel} ${circumference - fillLevel}`}
+                        strokeDashoffset={circumference / 6}
+
+                    />
+
                 </Svg>
-                
-                
-                 
+
+
+
             </View>
          </ScrollView>
     </View>);
--- src/core/components/AccountScreenComponents/styles/AccountContentStyles.js
+++ src/core/components/AccountScreenComponents/styles/AccountContentStyles.js
@@ -17,6 +17,7 @@ export default StyleSheet.create({
     },
     statsList:{
         width:'100%',
+        height:400,
     },
     goldHeader:{
         fontSize:RFValue(20),
