95a477a188745f55f484a7257a7e7ff5e76ecf5f
TalesOfANoob <44920433+TalesOfANoob@users.noreply.github.com>
2020-11-09 11:12:41 +0200

    l

---
+++ src/core/components/AccountScreenComponents/AccountContent.js
@@ -0,0 +1,83 @@
+/* eslint-disable prettier/prettier */
+import React,{useState,useEffect} from 'react';
+import {View,Text,ScrollView} from 'react-native';
+import {AccountContentStyle as styles} from './styles';
+import strings from './static/strings';
+import Svg,{Text as SvgText,Circle} from 'react-native-svg';
+import { RFValue } from 'react-native-responsive-fontsize';
+import Animated from 'react-native-reanimated';
+const AccountContent = () => {
+
+    const size = 65;
+    const strokeWidth = 15;
+    const radius = size - strokeWidth;
+    const circumference = 2 * Math.PI * radius;
+
+    const mockData = {
+        estadistics:{
+            level:30,
+            sales:10,
+            client:9,
+
+        },
+    };
+    const [level,setLevel] = useState(0);
+    useEffect(()=>{
+        setInterval(() => {
+
+            setLevel(l=>l < 100 ? l + 0.5 : 0);
+
+        }, 10);
+    },[]);
+    //Animated.timing(setLevel(l=>l < 100 ? l + 0.5 : 0))
+    const fillLevel = ( level * circumference) / 100;
+
+    return (
+    <View style={styles.container}>
+        <Text style={styles.goldHeader}>
+            {strings.findHome}
+        </Text>
+        <ScrollView
+
+            style={styles.statsList}
+         >
+            <View >
+                <Text
+                    style={styles.estatistics}>
+                    {strings.estadistics}
+                </Text>
+
+                <Svg style={{width:size * 2,height:size * 2}}>
+
+                    <Circle
+                        stroke="#e3e3e3"
+                        fill="none"
+                        r={radius}
+                        cx={size}
+                        cy={size}
+                        strokeWidth={strokeWidth}
+                        strokeDasharray={`${circumference}`}
+                    />
+                    <Circle
+
+                        stroke="#46D0D9"
+                        fill="none"
+                        r={radius}
+                        cx={size}
+                        cy={size}
+                        strokeWidth={strokeWidth}
+                        strokeDasharray={`${fillLevel} ${circumference - fillLevel}`}
+                        strokeDashoffset={circumference / 6}
+
+                    />
+
+                </Svg>
+
+
+
+            </View>
+         </ScrollView>
+    </View>);
+};
+
+export default AccountContent;
---
+++ src/core/components/AccountScreenComponents/styles/AccountContentStyles.js
@@ -0,0 +1,33 @@
+/* eslint-disable prettier/prettier */
+import {StyleSheet} from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+export default StyleSheet.create({
+
+    container:{
+        backgroundColor:'#e0f0ff',
+        width:'100%',
+        borderRadius:32,
+        paddingHorizontal:20,
+        paddingVertical:30,
+    },
+    statsContainer:{
+
+        
+    },
+    statsList:{
+        width:'100%',
+        height:400,
+    },
+    goldHeader:{
+        fontSize:RFValue(20),
+        fontWeight:'bold',
+        color:'#143656',
+    },
+    estatistics:{
+
+    },
+    info:{
+
+    },
+});
